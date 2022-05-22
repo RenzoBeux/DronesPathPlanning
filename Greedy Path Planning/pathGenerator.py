3f337c1ce2d4ff17caa98a0a741ad9aaf379be2a
Felipe Almeida <felipe.almeida@decemberlabs.com>
2022-05-22 15:36:15 -0300

    Isolation of heuristics

--- Greedy Path Planning/pathGenerator.py
+++ Greedy Path Planning/pathGenerator.py
@@ -9,16 +9,17 @@ def generatePOICoord():
     return coordObject(x,y)
 
 if __name__ =="__main__":
-    seed(1)
+    seed(0)
     base = coordObject(0,0)
     dims = DIM
     amountOfUAV = UAVAMOUNT
-    POIPosition = [coordObject(1,1)]
+    POIPosition = [coordObject(0.5,0.5)]
     POITimes = [10]
     POIList:list[POI] = []
     UAVList:list[UAV] = []
     needyPOI:list[POI] = []
     printMapGrid(base,POIPosition)
+    # POI and UAV creation
     for i in range(POIAMOUNT):
         currPOI = POI(POIPosition[i],POITimes[i])
         POIList.append(currPOI)
@@ -26,18 +27,15 @@ if __name__ =="__main__":
     for i in range(UAVAMOUNT):
         currUAV = UAV(dims,base)
         UAVList.append(currUAV)
+    # Creation of the routes
     for t in range(TIMELENGTH):
         print(UAVList[0].position.x,' ',UAVList[0].position.y)
+        for poi in POIList:
+            if (not(poi in needyPOI) and (t - poi.lastVisit > poi.expectedVisitTime)):
+                needyPOI.append()
         for uav in UAVList:
-            if uav.isOnTarget():
-                uav.removeTarget()
-        for poi in needyPOI:
-            for uav in UAVList:
-                if uav.isFree():
-                    uav.setTarget(poi)
-        for uav in UAVList:
-            uav.move()
-            printMapGrid(uav.position,POIPosition)
+            uav.move([t,dims,needyPOI])
+            printMapGrid(uav.position,list(map(lambda poi : poi.getSection(dims),POIList)))
     
     for uav in UAVList:
         print(list(map(lambda move: move.name,uav.moves)))
