f053d1d2365cf1db6bf7926e294cbded7dfbb852
Felipe <felipealmeida.id@gmail.com>
2022-06-04 18:20:27 -0300

    Fully functional, overlap of annotations on overlapping stays

--- Greedy Path Planning/routeDrawer.py
+++ Greedy Path Planning/routeDrawer.py
@@ -1,24 +1,47 @@
 import matplotlib.pyplot as plt
 from coordObject import coordObject
 from constants import ACTION
+from POI import POI
 
-def drawRoute(dim:coordObject, origin:coordObject, routes:list[list[ACTION]]):
+colors = ['b','g','r','c','m','k']
+markers = ['o','^','v']
+
+def drawRoute(dimensions:coordObject, Pois:list[POI], origin:coordObject, routes:list[list[ACTION]]):
   plt.figure()
+  plt.xlim(-0.3,dimensions.x+0.3)
+  plt.ylim(-0.3,dimensions.y+0.3)
+  plt.grid(True)
 
-  for route in routes:
+  for poi in Pois:
+      x = poi.getSection(dimensions).x
+      y = poi.getSection(dimensions).y
+      plt.plot(x,y,marker='o',color='k')
+
+  for index,route in enumerate(routes):
     position = coordObject(origin.x,origin.y)
     xCoordenates = []
     yCoordenates = []
-    for move in route:
+    for moveIndex,move in enumerate(route):
         xCoordenates.append(position.x)
         yCoordenates.append(position.y)
         position.x += xDelta(move)
-        position.y += yDelta(move)   
+        position.y += yDelta(move)
+        tempPos = plt.scatter(position.x,position.y,marker=markers[index],color=colors[index])
+        if move == ACTION.STAY:
+            plt.scatter(position.x,position.y,s=80,facecolors='none',edgecolors=colors[index])
+            if  moveIndex ==0 or route[moveIndex-1] != ACTION.STAY:
+                acc = 1
+                i = 1
+                while route[moveIndex + i] == ACTION.STAY:
+                    acc += 1
+                    i += 1
+                plt.annotate(acc,(position.x+0.1,position.y+0.1))
+        plt.plot(xCoordenates,yCoordenates,color=colors[index])
+        plt.pause(1)
+        tempPos.remove()
     xCoordenates.append(position.x)
     yCoordenates.append(position.y)
-    plt.plot(xCoordenates,yCoordenates)
-  plt.xlim(-0.3,dim.x+0.3)
-  plt.ylim(-0.3,dim.y+0.3)
+    plt.plot(xCoordenates,yCoordenates,color=colors[index])
   plt.show()
   return 0
 
@@ -43,8 +66,13 @@ def yDelta(move:ACTION):
     else:
         return 0
 
+def mapFun(coord:coordObject):
+    return POI(coord,0,0)
+
 if __name__ == '__main__':
-    routes = [[ACTION.UP,ACTION.RIGHT,ACTION.RIGHT,ACTION.DOWN,ACTION.RIGHT,ACTION.DIAG_UP_RIGHT],[ACTION.RIGHT]]
-    dim = coordObject(5,5)
+    routes = [[ACTION.UP,ACTION.STAY,ACTION.STAY,ACTION.RIGHT,ACTION.RIGHT,ACTION.DOWN,ACTION.RIGHT,ACTION.STAY,ACTION.DIAG_UP_RIGHT],[ACTION.UP,ACTION.STAY,ACTION.RIGHT,ACTION.UP,ACTION.UP]]
+    coordPoi = [coordObject(0.8,0.5),coordObject(0.75,0.9),coordObject(0.9,0)]
+    poi = list(map(mapFun,coordPoi))
+    dimensions = coordObject(5,5)
     origin = coordObject(0,0)
-    drawRoute(dim,origin,routes)
+    drawRoute(dimensions,poi,origin,routes)
