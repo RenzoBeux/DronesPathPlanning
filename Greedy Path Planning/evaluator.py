d462565da5403652a348edc6cdd6abc833a42596
Renzo Beux <renzobeux@gmail.com>
2022-06-11 17:56:58 -0300

    feat: evaluator evaluates coverage and collission

--- Greedy Path Planning/evaluator.py
+++ Greedy Path Planning/evaluator.py
@@ -1,4 +1,5 @@
 # this function parses the txt file and returns a list of lists
+from collections import Counter
 from constants import *
 
 
@@ -30,6 +31,9 @@ def populateArea(actions:list[list[ACTION]], areaDims:coordObject) -> list[list[
     #for each drone we will initialize his current point in 0,0
     for i in range(len(actions)):
         currentPos.append(coordObject(0,0))
+    #we mark the initial point of each drone as visited
+    for i in range(len(actions)):
+        res[currentPos[i].x][currentPos[i].y].append(0)
     #for each drone
     for i in range(len(actions)):
         # we will iterate through the actions
@@ -58,21 +62,49 @@ def populateArea(actions:list[list[ACTION]], areaDims:coordObject) -> list[list[
             res[currentPos[i].x ][currentPos[i].y ].append(j)
     return res
 
+def get_duplicates(array):
+    c = Counter(array)
+    #return a dictonary
+    return {k: v for k, v in c.items() if v > 1}
+
+################################################################################################################################################
+########################################################## QUALITY EVALUATOR FUNCTIONS #########################################################
+################################################################################################################################################
+
 #This function evaluates the coverage of the area
 def evaluateCoverageArea(actions:list[list[ACTION]], areaDims:coordObject) -> float:
     area = populateArea(actions, areaDims)
-    res = 100
+    numberOfSquares = areaDims.x * areaDims.y
+    res = numberOfSquares
     for i in range(areaDims.x):
         for j in range(areaDims.y):
             if len(area[i][j]) == 0:
                 res = res - 1
-    numberOfSquares = areaDims.x * areaDims.y
-    return res / numberOfSquares
+    return res / numberOfSquares *100
+
+#This function will punish if drones share the same square at the same time
+def evaluateDronesCollision(actions:list[list[ACTION]], areaDims:coordObject) -> float:
+    area = populateArea(actions, areaDims)
+    numberOfDrones = len(actions)
+    numberOfTimes = len(actions[0])
+    worstCase = numberOfDrones * numberOfTimes #all time moves together
+    res = 0
+    for i in range(areaDims.x):
+        for j in range(areaDims.y):
+            #find the repeted times in the list
+            duplicates = get_duplicates(area[i][j])
+            for k in duplicates:
+                res = res + duplicates[k]
+
+            
+    return 100 - (res / worstCase * 100)
+
 
 def evaluate(grid:list[list[ACTION]]):
     print(evaluateCoverageArea(grid,coordObject(5, 5)))
+    print(evaluateDronesCollision(grid,coordObject(5, 5)))
 
 
-lista = parseFile("output/90/1.txt")
+lista = parseFile('output/90/1.txt')
 renderedList = parseMoves(lista)
 evaluate(renderedList)
