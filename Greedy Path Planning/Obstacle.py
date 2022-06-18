82a2e70edca8573310e64979ae0451b30a4903d9
Renzo Beux <renzobeux@gmail.com>
2022-06-18 17:44:42 -0300

    feat: added obnstacles

---
+++ Greedy Path Planning/Obstacle.py
@@ -0,0 +1,25 @@
+from coordObject import coordObject
+from constants import *
+
+
+class Obstacle:
+
+    def __init__(self, dimsInit: coordObject, dimsEnd: coordObject, id: int):
+        self.dimsInit = dimsInit
+        self.dimsEnd = dimsEnd
+        self.id = id
+
+    def toSections(self, gridDimensions:coordObject) -> list[coordObject]:
+        sections = []
+        percentageOfSectionInitX = gridDimensions.x * self.dimsInit.x
+        percentageOfSectionEndX = gridDimensions.x * self.dimsEnd.x
+        percentageOfSectionInitY = gridDimensions.y * self.dimsInit.y
+        percentageOfSectionEndY = gridDimensions.y * self.dimsEnd.y
+        sectionInitX = int(percentageOfSectionInitX)
+        sectionEndX = int(percentageOfSectionEndX)
+        sectionInitY = int(percentageOfSectionInitY)
+        sectionEndY = int(percentageOfSectionEndY)
+        for i in range(sectionInitX, sectionEndX + 1):
+            for j in range(sectionInitY, sectionEndY + 1):
+                sections.append(coordObject(i, j))
+        return sections
