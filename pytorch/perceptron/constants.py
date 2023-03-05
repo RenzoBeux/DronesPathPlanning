cc4f5f47a915cd7bd02494656b42deeac133d9f0
Renzo Beux <renzobeux@gmail.com>
2023-03-05 17:16:03 -0300

    restructure

---
+++ pytorch/perceptron/constants.py
@@ -0,0 +1,49 @@
+from os import listdir
+from torch import device,cuda, tensor
+
+class Constants_Class(object):
+  def __new__(cls):
+    if not hasattr(cls, 'instance'):
+      cls.instance = super(Constants_Class, cls).__new__(cls)
+    return cls.instance
+
+  def __init__(self):
+    self.set_uav_amount_and_time()
+    self.set_device()
+
+  BATCH_SIZE = 32
+  EPOCHS = 256
+  sample_size = 3 # fixed sample size
+  NOISE_DIM = 128 # latent vector size
+  K = 1 # number of steps to apply to the discriminator
+  device
+  uav_amount:int = None
+  time_lenght:int = None
+  g_learn_rate:float = 0.0002
+  d_learn_rate:float = 0.000005
+
+  def set_uav_amount_and_time(self):
+    input_files = listdir('./input')
+    single_file = listdir(f"./input/{input_files[0]}")
+    file = open(f"./input/{input_files[0]}/{single_file[0]}",'r')
+    file_lines = file.readlines()
+    file.close()
+    file_routes = list(map(lambda x : [list(map(int, x.split(' ')))],file_lines))
+    files_tensor_routes = tensor(file_routes,dtype=int)
+    self.uav_amount = files_tensor_routes.shape[0]
+    self.time_lenght =files_tensor_routes.shape[2]
+
+  def set_device(self):
+    self.device= device('cuda' if cuda.is_available() else 'cpu')
+
+constants = Constants_Class()
+
+
+
+
+
+
+
+
+
+
